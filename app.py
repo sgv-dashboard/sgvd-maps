3e623a5fcf528b9c333912ff80b4c256188555b1
siemenvdv <siemen.vandervoort@student.uhasselt.be>
2021-12-27 11:41:04 +0100

    Vertaling ned

--- app.py
+++ app.py
@@ -6,38 +6,51 @@ import folium
 
 app = Flask(__name__)
 
-
-
 ###############################################################
 #                         Web Routes                         #
 ###############################################################
 
 @app.route('/')
 def index():
+	return render_template('index.html')
+
+@app.route('/map', methods=['GET'])
+def get_map():
+	#Key hoort bij mijn account, max 2000 requests per 24h
 	client = openrouteservice.Client(key='5b3ce3597851110001cf62484b7bc6e27b5b47fabce3821209f35d73')
-	coords = ((80.21787585263182,6.025423265401452),(80.23990263756545,6.018498276842677))
+
+	#Test url: http://127.0.0.1:5000/map?latS=51.075824&lonS=5.262364&latE=50.927683&lonE=5.386107
+	latitudeStart = float(request.args.get('latS'))
+	longitudeStart = float(request.args.get('lonS'))
+	latitudeEnd = float(request.args.get('latE'))
+	longitudeEnd = float(request.args.get('lonE'))
+
+	coords = ((longitudeStart, latitudeStart),(longitudeEnd, latitudeEnd))
 	res = client.directions(coords)
 	geometry = client.directions(coords)['routes'][0]['geometry']
 	decoded = convert.decode_polyline(geometry)
 
-	distance_txt = "<h4> <b>Distance :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['distance']/1000,1))+" Km </strong>" +"</h4></b>"
-	duration_txt = "<h4> <b>Duration :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['duration']/60,1))+" Mins. </strong>" +"</h4></b>"
+	#We kunnen ook gewoon de afstand en tijd weergeven, zonder kaartje
+	distance = str(round(res['routes'][0]['summary']['distance']/1000,1))
+	duration = str(round(res['routes'][0]['summary']['duration']/60,1))
 
-	m = folium.Map(location=[6.074834613830474, 80.25749815575348],zoom_start=10, control_scale=True,tiles="cartodbpositron")
+	distance_txt = "<h4> <b>Afstand :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['distance']/1000,1))+" Km </strong>" +"</h4></b>"
+	duration_txt = "<h4> <b>Tijd :&nbsp" + "<strong>"+str(round(res['routes'][0]['summary']['duration']/60,1))+" Mins. </strong>" +"</h4></b>"
+
+	m = folium.Map(location=[longitudeStart, latitudeStart],zoom_start=10, control_scale=True,tiles="cartodbpositron")
 	folium.GeoJson(decoded).add_child(folium.Popup(distance_txt+duration_txt,max_width=300)).add_to(m)
 
 	folium.Marker(
 		location=list(coords[0][::-1]),
-		popup="Galle fort",
+		popup="Vertrek",
 		icon=folium.Icon(color="green"),
 	).add_to(m)
 
 	folium.Marker(
 		location=list(coords[1][::-1]),
-		popup="Jungle beach",
+		popup="Bestemming",
 		icon=folium.Icon(color="red"),
 	).add_to(m)
-
 
 	m.save('templates/map.html')
 	return render_template("map.html")
