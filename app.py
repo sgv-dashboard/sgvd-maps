50f04d2e1394a54d5e9216645bd69408e48ae4d1
siemenvdv <siemen.vandervoort@student.uhasselt.be>
2021-12-30 11:54:57 +0100

    CORS

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from datetime import datetime
 import openrouteservice
 from openrouteservice import convert
 import folium
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 ###############################################################
 #                         Web Routes                         #
